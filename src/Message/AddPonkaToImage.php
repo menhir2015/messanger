9a1afbfcd4fec5b44678893943d9c23abc9c2036
Andrea Menna <andrea.menna@blujaysolutions.com>
2022-11-27 10:58:51 +0100

    3 step

--- src/Message/AddPonkaToImage.php
+++ src/Message/AddPonkaToImage.php
@@ -2,7 +2,26 @@
 
 namespace App\Message;
 
+use App\Entity\ImagePost;
+
 class AddPonkaToImage
 {
+
+    private $imagePost;
+
+    public function __construct(ImagePost $imagePost) {
+        $this->imagePost = $imagePost;
+    }
+
+    /**
+     * @return \App\Entity\ImagePost
+     */
+    public function getImagePost() : ImagePost
+    {
+        return $this->imagePost;
+    }
+
+
+
 
 }
